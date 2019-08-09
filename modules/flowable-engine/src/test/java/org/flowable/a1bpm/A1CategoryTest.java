0cfd4b5099eb047c92b5f6679e06b4c4846d27ce
shareniu <1515308@qq.com>
2019-08-09 14:41:56 +0800

    测试代码

---
+++ modules/flowable-engine/src/test/java/org/flowable/a1bpm/A1CategoryTest.java
@@ -0,0 +1,40 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.flowable.a1bpm;
+
+import org.flowable.bpmn.model.BpmnModel;
+import org.flowable.engine.impl.test.ResourceFlowableTestCase;
+import org.flowable.engine.runtime.ProcessInstance;
+import org.flowable.engine.test.Deployment;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author shareniu
+ */
+public class A1CategoryTest extends ResourceFlowableTestCase {
+
+    public A1CategoryTest() {
+        super("org/flowable/a1bpm/simple/flowable.cfg.xml");
+    }
+    //A1CategoryTest.testSimple.bpmn20.xml
+    @Test
+ @Deployment
+    public void testSimple() {
+                ProcessInstance pi = runtimeService.startProcessInstanceByKey("f");
+        String processDefinitionId = pi.getProcessDefinitionId();
+        BpmnModel bpmnModel = repositoryService.getBpmnModel(processDefinitionId);
+        System.out.println(bpmnModel);
+    }
+
+}
